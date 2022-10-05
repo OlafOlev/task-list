7b7cd2129a2c2fabf0d8656a23767189ed639a1e
olaf <oalfolev@gmail.com>
2022-10-05 10:44:49 +0300

    As a user i can reload page and see actual data [Delivers #183440372]

--- app.js
+++ app.js
@@ -1,72 +1,74 @@
-const form = document.querySelector('form')
+const form  = document.querySelector('form')
 const taskInput = document.querySelector('#task')
 const taskList = document.querySelector('ul')
-const deletealltask = document.querySelector('#del-tasks')
+const delAllTasks = document.querySelector('#del-tasks')
 
-// const myTask = document.querySelector('#task-title ul li')
-taskList.addEventListener('click', deleteTask)
 form.addEventListener('submit', addTask)
-deletealltask.addEventListener('click', deleteAllTask)
-
+taskList.addEventListener('click', deleteTask)
+delAllTasks.addEventListener('click', deleteAllTasks)
 document.addEventListener('DOMContentLoaded', getTasks)
 
-
 function addTask(e){
-    //create list item
+    // create list item
     const li = document.createElement('li')
     li.appendChild(document.createTextNode(taskInput.value))
     li.className = 'collection-item'
     const a = document.createElement('a')
-    a.appendChild(document.createTextNode('x'))
+    a.appendChild(document.createTextNode('X'))
     a.className = 'blue-text text-darken-2 secondary-content'
     a.setAttribute('href', '#')
     li.appendChild(a)
-    //add to list
+    // add to list
     const ul = document.querySelector('ul')
     ul.appendChild(li)
     addTaskLS(taskInput.value)
     taskInput.value = ''
     e.preventDefault()
 }
+
 function deleteTask(e){
     if(e.target.textContent == 'X'){
-        if(confirm('Aru you sure to delete this task')){
+        if(confirm('Are you sure to delete this task?')){
             e.target.parentElement.remove()
             deleteTaskLS(e.target.parentElement.textContent.slice(0,-1))
         }
     }
 }
-function deleteAllTask(e){
-    while(taskList.firstChild){
-        taskList.removeChild(taskList.firstChild)
 
+function deleteAllTasks(e){
+    //taskList.innerHTML = ''
+    while (taskList.firstChild){
+        taskList.removeChild(taskList.firstChild)
     }
+    localStorage.removeItem('tasks')
 }
 
-function addTaskLS(task){
+function addTaskLS(task) {
     let tasks
     if(localStorage.getItem('tasks') === null){
         tasks = []
-    }else{
+    } else {
         tasks = JSON.parse(localStorage.getItem('tasks'))
     }
     tasks.push(task)
     localStorage.setItem('tasks', JSON.stringify(tasks))
 }
-function  deleteTaskLS(task){
+
+function deleteTaskLS(task) {
     let tasks
     if(localStorage.getItem('tasks') === null){
         tasks = []
-    }else {
+    } else {
         tasks = JSON.parse(localStorage.getItem('tasks'))
     }
     tasks.forEach((taskLS, taskIndex) => {
         if(taskLS === task){
-            task.splice(taskIndex, 1)
+            tasks.splice(taskIndex, 1)
         }
     })
     localStorage.setItem('tasks', JSON.stringify(tasks))
 }
+
 function getTasks(){
     let tasks
     if(localStorage.getItem('tasks') === null){
@@ -79,11 +81,11 @@ function getTasks(){
         li.appendChild(document.createTextNode(task))
         li.className = 'collection-item'
         const a = document.createElement('a')
-        a.appendChild(document.createTextNode('x'))
+        a.appendChild(document.createTextNode('X'))
         a.className = 'blue-text text-darken-2 secondary-content'
         a.setAttribute('href', '#')
         li.appendChild(a)
-        //add to list
+        // add to list
         const ul = document.querySelector('ul')
         ul.appendChild(li)
     })
